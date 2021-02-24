396b389f40d483212379eba665aa45d8c05eda08
Alfons Lorente <alorente@cifpfbmoll.eu>
2021-02-24 17:55:33 +0100

    Clase PuntuacioTest preparada
    S'ha preparat la clase PuntuacioTest per el seu funcionament

---
+++ src/test/PuntuacioTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import com.jaume.penjat.Puntuacio;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PuntuacioTest {
+
+    private Puntuacio puntuacio = new Puntuacio();
+
+}
