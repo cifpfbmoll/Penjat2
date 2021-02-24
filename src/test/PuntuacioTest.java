546642d299e618941fdc39ed815d8efeed541da2
Alfons Lorente <alorente@cifpfbmoll.eu>
2021-02-24 17:59:16 +0100

    Fet el test getParaulaSecretaDificultatNumeroForaDeRang
    S'ha fet un test del metode getParaulaSecretaDificultat que comproba que pasa si el parametre es un nombre fora del rang
    Tasca2

--- src/test/PuntuacioTest.java
+++ src/test/PuntuacioTest.java
@@ -7,5 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class PuntuacioTest {
 
     private Puntuacio puntuacio = new Puntuacio();
+    @Test
+    void getParaulaSecretaDificultatNumeroForaDeRang() {
+        assertEquals("err", puntuacio.getParaulaSecretaDificultat(4), "Ha hagut un error, s'esperava: err");
+    }
 
 }
